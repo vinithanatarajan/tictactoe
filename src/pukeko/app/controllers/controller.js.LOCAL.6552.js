a5b6dfc6427fe123bc001145cd3bd89d0ece777b
u <u@gmail.com>
2015-02-20 14:33:25 +1300

    resetting this

--- src/pukeko/app/controllers/controller.js.LOCAL.6552.js
+++
@@ -1,9 +0,0 @@
-function Controller() {
-
-    this.view = new View();
-        this.view.clearScreen();
-
-    };
-Controller.prototype.startGame = function() {
-        this.view.showBoard();
-    };
